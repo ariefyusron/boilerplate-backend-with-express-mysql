f70cb398979356dd84d746ca5d9d8780a9672976
Arief Yusron <ariefyusron98@gmail.com>
2019-02-24 22:56:10 +0700

    set modules

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const bodyParser = require('body-parser');
+const validator = require('express-validator');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,10 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 app.use((req,res,next) => {
   req.jwt = jwt
+  req.secret_key = process.env.JWT_SECRET_KEY
+  req.bcrypt = bcrypt
+  req.saltRounds = saltRounds
   next()
-})
+});
+app.use(validator());
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter)
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 exports.checkAuth = (req,res,next) => {
   try{
     const token = req.headers.authorization.split(' ')[1]
-    const decoded = req.jwt.verify(token,secret_key)
+    const decoded = req.jwt.verify(token,req.secret_key)
     req.userData = decoded.showUser
     next()
   } catch(error){
